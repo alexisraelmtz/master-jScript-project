7c25a5aa2e35e783a86b83fb3fa3d02326e9eea2
alexisraelmtz <alex.israel.mtz@gmail.com>
2021-06-18 04:30:28 -0500

    Styling and CSS, Background ready -deck almost finished

--- deck-class/app/deck.js
+++ deck-class/app/deck.js
@@ -1,6 +1,6 @@
 class Deck {
     numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
-    suits = ['♣', '♦', '♥', '♠'];
+    suits = ['♣', '♥', '♠', '♦'];
     cards = [];
 
     constructor() {
