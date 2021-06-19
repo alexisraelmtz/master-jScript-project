f9d01893bbae094a4dba66708223235d66d51d9f
alexisraelmtz <alex.israel.mtz@gmail.com>
2021-06-19 01:29:33 -0500

    Issue: #01 - Fixed + Issue: #02 Card Styling - pending

--- deck-class/server-run.js
+++ deck-class/server-run.js
@@ -2,7 +2,7 @@ const { Deck, Hand } = require('./app/deck.js');
 const express = require('express');
 
 const app = express();
-const port = 4001;
+const port = 4000;
 // app.use(cors());
 
 app.use(express.static('public'));
